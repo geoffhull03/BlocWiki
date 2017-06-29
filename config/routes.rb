389d0957e936b9ebf90cb5a199229e371e1d741d
Geoff Hull <geoffhull03@gmail.com>
2017-06-28 18:02:14 -0700

    Added Sign Up and In Abilities

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/name'
+
+  get 'user/email'
+
   devise_for :users
   get "welcome/index"
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe "attributes" do
+    it "should have name and email attributes" do
+      expect(user).to have_attributes(name: user.name, email: user.email)
+    end
+  end
 end
