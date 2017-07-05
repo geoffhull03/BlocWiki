ac15721b690c7eda9723d7297acbb31d4be8c35d
Geoff Hull <geoffhull03@gmail.com>
2017-07-05 14:30:39 -0700

    Markdown Syntax Available to Use

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -2,4 +2,9 @@ module WikisHelper
   def is_authorized
     current_user.role == "premium"
   end
+
+  def markdown(text)
+        extensions = [:tables, :bold, :fenced_code_blocks, :autolink, :strikethrough, :underline, :highlight, :quote, :superscript, :footnotes]
+        Markdown.new(text, *extensions).to_html.html_safe
+  end
 end
